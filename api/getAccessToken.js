dbf2db9dbbd34bb3c2d5f403e6303c43c5e10f15
playersgate <yuji.sato@playersgate.jp>
2025-05-23 12:59:29 +0900

    Add files via upload

--- api/getAccessToken.js
+++ api/getAccessToken.js
@@ -2,15 +2,13 @@ const axios = require("axios");
 
 async function getAccessToken() {
     try {
-        const response = await axios.post("https://api.freee.co.jp/auth/token", {
-            grant_type: "refresh_token",
-            refresh_token: process.env.FREEE_REFRESH_TOKEN,
+        const response = await axios.post("https://accounts.secure.freee.co.jp/public_api/token", {
+            grant_type: "authorization_code",
+            code: process.env.FREEE_AUTH_CODE,
+            redirect_uri:process.env.FREEE_REDIRECT_URI,
             client_id: process.env.FREEE_CLIENT_ID,
             client_secret: process.env.FREEE_CLIENT_SECRET
         });
-        console.error("FREEE_REFRESH_TOKEN:",process.env.FREEE_REFRESH_TOKEN);
-        console.error("FREEE_CLIENT_ID:",process.env.FREEE_CLIENT_ID);
-        console.error("FREEE_CLIENT_SECRET:",process.env.FREEE_CLIENT_SECRET);
         return response.data.access_token;
     } catch (error) {
         console.error("❌ アクセストークン取得失敗:", error.response?.data || error.message);
