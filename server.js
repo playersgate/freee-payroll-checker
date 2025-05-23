74372754d9badad02eb540430418522f97e9ebb8
playersgate <yuji.sato@playersgate.jp>
2025-05-23 16:29:05 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { checkPayrollErrors } = require("./api/checkPayroll");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.static("public"));
 
